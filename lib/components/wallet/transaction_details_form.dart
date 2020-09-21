7387c88fe26725380f26658e8f6272a15e00bb20
Raul Jareño <raul.jareno.diaz@gmail.com>
2020-09-21 17:39:17 +0200

    added tx details screen customization

--- lib/components/wallet/transaction_details_form.dart
+++ lib/components/wallet/transaction_details_form.dart
@@ -1,12 +1,6 @@
-import 'package:flutter/services.dart';
-import 'package:hermez/components/form/address_input.dart';
-import 'package:hermez/components/form/amount_input.dart';
-import 'package:hermez/components/form/paper_form.dart';
-import 'package:hermez/components/form/paper_validation_summary.dart';
-import 'package:hermez/components/wallet/account_row.dart';
-import 'package:hermez/context/transfer/wallet_transfer_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hermez/utils/hermez_colors.dart';
 import 'package:hermez/wallet_transfer_amount_page.dart';
 
 class TransferSummaryForm extends HookWidget {
@@ -27,10 +21,8 @@ class TransferSummaryForm extends HookWidget {
   @override
   Widget build(BuildContext context) {
     //final toController = useTextEditingController(text: token);
-    final amountController = useTextEditingController();
     final addressController = useTextEditingController();
-
-
+    final amountController = useTextEditingController();
     //final transferStore = useWalletTransfer(context);
 
     /*useEffect(() {
@@ -38,220 +30,173 @@ class TransferSummaryForm extends HookWidget {
       return null;
     }, [token]);*/
 
-    return Container(
-      color: Colors.white,
-          child:
-              ListView(
-                children: ListTile.divideTiles(
-                    context: context,
-                    color: Colors.black54,
-                    tiles: [
-                      _buildAmountRow(context, null, amountController),
-                      transactionType == TransactionType.DEPOSIT ?
-                       Container(): ListTile(
-                        title: Text('Status',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                            )),
-                        trailing: Text('Confirmed',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                            )),
-                      ),
-                      ListTile(
-                        contentPadding: EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
-                        title: Column(children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                            Text('From',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.w500,
-                                )),
-                              Align(
-                                alignment: Alignment.centerRight,
-                              child:
-                                Text('My Ethereum address',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                            )
-                          ],),
-                          SizedBox(height: 7),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child:
-                            Text('0x8D70...461B5',
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                          ))
-                        ],),
-                      ),
-                      ListTile(
-                        contentPadding: EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
-                        title: Column(children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text('To',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.w500,
-                                  )),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child:
-                                Text('My Hermez address',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.w600,
-                                    )),
-                              )
-                            ],),
-                          SizedBox(height: 7),
-                          Align(
-                              alignment: Alignment.centerRight,
-                              child:
-                              Text('hez:0x8D70...461B5',
-                                style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ))
-                        ],),
-                      ),
-                      ListTile(
-                        contentPadding: EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
-                        title: Column(children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text('Fee',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.w500,
-                                  )),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child:
-                                Text('€0.1',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.w600,
-                                    )),
-                              )
-                            ],),
-                          SizedBox(height: 7),
-                          Align(
-                              alignment: Alignment.centerRight,
-                              child:
-                              Text('0.119231 USDT',
-                                style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ))
-                        ],),
-                      ),
-                      transactionDate != null ?
-                      ListTile(
-                        title: Text('Date',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                        )),
-                        trailing: Text('17 Aug 2020',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                            )),
-                      ) : Container(),
-                      transactionDate == null ?
-                       Column(
-                    children: <Widget>[
-                      SizedBox(height: 20),
-                      FlatButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40.0),
-                            side: BorderSide(color: Color.fromRGBO(211, 87, 46, 1.0))),
-                        onPressed: () {
-                          this.onSubmit(
-                            //toController.value.text,
-                            amountController.value.text,
-                            amountController.value.text,
-                          );
-                        },
-                        padding: EdgeInsets.all(15.0),
-                        color: Color.fromRGBO(211, 87, 46, 1.0),
-                        textColor: Colors.white,
-                        child: Text("Send",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                            )),
-                      ),
-                      SizedBox(height: 20),
-                  ]) : Container()
-                    ]
-                ).toList(),
-              ),
-    );
-  }
-
-  Widget _buildAmountRow(BuildContext context, dynamic element, dynamic amountController) {
-    // returns a row with the desired properties
-    return Container(
-      color: Color.fromRGBO(243, 243, 243, 1.0),
-        padding: EdgeInsets.only(bottom: 15.0),
-        child: ListTile(
-              title: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 10.0),
-                    child:
-                    Text("€5",
-                      textAlign: TextAlign.center,
+    return Column(
+        children: ListTile.divideTiles(
+            context: context,
+            color: HermezColors.blueyGreyThree,
+            tiles: [
+          transactionType == TransactionType.DEPOSIT
+              ? Container()
+              : ListTile(
+                  title: Text('Status',
                       style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 48.0,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    )
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 15.0, bottom: 30.0),
-                    child: Text("5,899510 USDT",
-                        textAlign: TextAlign.center,
+                        color: HermezColors.blackTwo,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w500,
+                      )),
+                  trailing: Text('Confirmed',
+                      style: TextStyle(
+                        color: HermezColors.black,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+          ListTile(
+            contentPadding:
+                EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
+            title: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('From',
                         style: TextStyle(
-                          color: Color.fromRGBO(130, 130, 130, 1.0),
-                          fontSize: 18.0,
+                          color: HermezColors.blackTwo,
+                          fontSize: 16,
+                          fontFamily: 'ModernEra',
                           fontWeight: FontWeight.w500,
-                        ),
+                        )),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text('My Ethereum address',
+                          style: TextStyle(
+                            color: HermezColors.black,
+                            fontSize: 16,
+                            fontFamily: 'ModernEra',
+                            fontWeight: FontWeight.w700,
+                          )),
+                    )
+                  ],
+                ),
+                SizedBox(height: 7),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      '0x8D70...461B5',
+                      style: TextStyle(
+                        color: HermezColors.blueyGreyTwo,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w500,
                       ),
-                    ),
-                ],
-              ), //title to be name of the crypto
-            )
-        );
+                    ))
+              ],
+            ),
+          ),
+          ListTile(
+            contentPadding:
+                EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
+            title: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('To',
+                        style: TextStyle(
+                          color: HermezColors.blackTwo,
+                          fontSize: 16,
+                          fontFamily: 'ModernEra',
+                          fontWeight: FontWeight.w500,
+                        )),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text('My Hermez address',
+                          style: TextStyle(
+                            color: HermezColors.black,
+                            fontSize: 16,
+                            fontFamily: 'ModernEra',
+                            fontWeight: FontWeight.w700,
+                          )),
+                    )
+                  ],
+                ),
+                SizedBox(height: 7),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'hez:0x8D70...461B5',
+                      style: TextStyle(
+                        color: HermezColors.blueyGreyTwo,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ))
+              ],
+            ),
+          ),
+          ListTile(
+            contentPadding:
+                EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
+            title: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Fee',
+                        style: TextStyle(
+                          color: HermezColors.blackTwo,
+                          fontSize: 16,
+                          fontFamily: 'ModernEra',
+                          fontWeight: FontWeight.w500,
+                        )),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text('€0.1',
+                          style: TextStyle(
+                            color: HermezColors.black,
+                            fontSize: 16,
+                            fontFamily: 'ModernEra',
+                            fontWeight: FontWeight.w700,
+                          )),
+                    )
+                  ],
+                ),
+                SizedBox(height: 7),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      '0.119231 USDT',
+                      style: TextStyle(
+                        color: HermezColors.blueyGreyTwo,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ))
+              ],
+            ),
+          ),
+          transactionDate != null
+              ? ListTile(
+                  title: Text('Date',
+                      style: TextStyle(
+                        color: HermezColors.blackTwo,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w500,
+                      )),
+                  trailing: Text('17 Aug 2020',
+                      style: TextStyle(
+                        color: HermezColors.black,
+                        fontSize: 16,
+                        fontFamily: 'ModernEra',
+                        fontWeight: FontWeight.w700,
+                      )),
+                )
+              : Container(),
+        ]).toList());
   }
 }
--- lib/wallet_transaction_details_page.dart
+++ lib/wallet_transaction_details_page.dart
@@ -1,11 +1,7 @@
-import 'package:hermez/components/wallet/transfer_form.dart';
-import 'package:hermez/context/transfer/wallet_transfer_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hermez/utils/hermez_colors.dart';
 import 'package:hermez/wallet_transfer_amount_page.dart';
 
-import 'components/wallet/loading.dart';
-import 'components/wallet/transfer_amount_form.dart';
 import 'components/wallet/transaction_details_form.dart';
 
 class TransactionDetailsArguments {
@@ -17,38 +13,133 @@ class TransactionDetailsArguments {
 }
 
 class TransactionDetailsPage extends StatefulWidget {
-  TransactionDetailsPage({Key key, this.arguments}) : super(key : key);
+  TransactionDetailsPage({Key key, this.arguments}) : super(key: key);
 
   final TransactionDetailsArguments arguments;
 
   @override
   _TransactionDetailsPageState createState() => _TransactionDetailsPageState();
 }
-class _TransactionDetailsPageState extends State<TransactionDetailsPage> {
 
+class _TransactionDetailsPageState extends State<TransactionDetailsPage> {
   @override
   Widget build(BuildContext context) {
+    // final amountController = useTextEditingController();
     //var transferStore = useWalletTransfer(context);
     //var qrcodeAddress = useState();
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(243, 243, 243, 1),
-        title: Text(widget.arguments.amountType == TransactionType.DEPOSIT ?  "Deposit" : "Send"),
+      appBar: new AppBar(
+        title: new Text(
+            widget.arguments.amountType == TransactionType.DEPOSIT
+                ? "Deposit"
+                : "Send",
+            style: TextStyle(
+                fontFamily: 'ModernEra',
+                color: HermezColors.blackTwo,
+                fontWeight: FontWeight.w800,
+                fontSize: 20)),
+        centerTitle: true,
         elevation: 0.0,
+        backgroundColor: HermezColors.lightOrange,
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.close),
+            onPressed: () => Navigator.of(context).pop(null),
+          ),
+        ],
       ),
-      body:  TransferSummaryForm(
+      body: Column(
+        children: [
+          _buildAmountRow(),
+          Expanded(
+            child: TransferSummaryForm(
               token: widget.arguments.token,
               transactionType: widget.arguments.amountType,
               onSubmit: (address, amount) async {
                 //var success = await transferStore.transfer(address, amount);
                 //Navigator.pushNamedAndRemoveUntil(context, "/", (Route<dynamic> route) => false);
-                Navigator.of(context).pushReplacementNamed( "/transaction_info");
+                Navigator.of(context).pushReplacementNamed("/transaction_info");
                 //if (success) {
-                  //Navigator.popUntil(context, ModalRoute.withName('/'));
+                //Navigator.popUntil(context, ModalRoute.withName('/'));
                 //}
               },
             ),
+          ),
+          //transactionDate == null
+          /* ? */ Column(children: <Widget>[
+            Container(
+              height: 52,
+              margin: EdgeInsets.only(
+                  left: 16.0, right: 16.0, top: 20.0, bottom: 20.0),
+              width: double.infinity,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14.0),
+                    side: BorderSide(color: HermezColors.darkOrange)),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushReplacementNamed("/transaction_info");
+                  //this.onSubmit(
+                  //amountController.value.text,
+                  //amountController.value.text,
+                  //addressController.value.text);
+                },
+                disabledColor: HermezColors.blueyGreyTwo,
+                padding: EdgeInsets.all(18.0),
+                color: HermezColors.darkOrange,
+                textColor: Colors.white,
+                child: Text("Send",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'ModernEra',
+                      fontWeight: FontWeight.w700,
+                    )),
+              ),
+            ),
+          ])
+          // : Container()
+        ],
+      ),
     );
+  }
+
+  Widget _buildAmountRow() {
+    // returns a row with the desired properties
+    return Container(
+        color: HermezColors.lightOrange,
+        padding: EdgeInsets.only(bottom: 15.0),
+        child: ListTile(
+          title: Column(
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    "€26.31",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: HermezColors.blackTwo,
+                      fontSize: 32.0,
+                      fontFamily: 'ModernEra',
+                      fontWeight: FontWeight.w800,
+                    ),
+                  )),
+              Container(
+                margin: EdgeInsets.only(top: 15.0, bottom: 30.0),
+                child: Text(
+                  "24.56 USDT",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: HermezColors.steel,
+                    fontSize: 18,
+                    fontFamily: 'ModernEra',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ), //title to be name of the crypto
+        ));
   }
 }
