c3fb0381647798b526222a6198cae4ee134e0566
Raul Jareño <raul.jareno.diaz@gmail.com>
2020-10-09 17:09:11 +0200

    loading progress on the list, not all view

--- lib/wallet_account_details_page.dart
+++ lib/wallet_account_details_page.dart
@@ -33,141 +33,118 @@ class WalletAccountDetailsPage extends HookWidget {
         arguments.store.state.defaultCurrency.toString().split('.').last;
 
     return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Expanded(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(arguments.element.publicKey, // name
-                        style: TextStyle(
-                            fontFamily: 'ModernEra',
-                            color: HermezColors.blackTwo,
-                            fontWeight: FontWeight.w800,
-                            fontSize: 20))
-                  ],
-                )),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(51, 51, 51, 1.0),
-                      border: Border.all(
-                        color: Color.fromRGBO(51, 51, 51, 1.0),
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                  padding:
-                      EdgeInsets.only(left: 10, right: 10, top: 7, bottom: 5),
-                  child: Text(
-                      arguments.store.state.txLevel == TransactionLevel.LEVEL1
-                          ? "L1"
-                          : "L2",
+      key: _scaffoldKey,
+      appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(arguments.element.publicKey, // name
                       style: TextStyle(
                           fontFamily: 'ModernEra',
-                          color: HermezColors.lightOrange,
-                          backgroundColor: Color.fromRGBO(51, 51, 51, 1.0),
+                          color: HermezColors.blackTwo,
                           fontWeight: FontWeight.w800,
-                          fontSize: 18)),
-                )
-              ],
-            ),
-            backgroundColor: HermezColors.lightOrange,
-            elevation: 0),
-        body: FutureBuilder<List<dynamic>>(
-            future: getTransactions(),
-            builder:
-                (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Container(
-                  color: Colors.white,
-                  child: Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: HermezColors.orange,
+                          fontSize: 20))
+                ],
+              )),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(51, 51, 51, 1.0),
+                    border: Border.all(
+                      color: Color.fromRGBO(51, 51, 51, 1.0),
                     ),
-                  ),
-                );
-              } else {
-                if (snapshot.hasError) {
-                  // while data is loading:
-                  return Container(
-                    color: Colors.white,
-                    child: Center(
-                      child: Text(
-                          'There was an error:' + snapshot.error.toString()),
-                    ),
-                  );
-                } else {
-                  if (snapshot.hasData) {
-                    // data loaded:
-                    //supportedTokens = snapshot.data;
-
-                    return Container(
-                      color: HermezColors.lightOrange,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          SizedBox(height: 40),
-                          SizedBox(
-                              width: double.infinity,
-                              child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(
-                                        (currency == "USD" ? "\$" : "€") +
-                                            ((currency == "USD"
-                                                        ? arguments.element.USD
-                                                        : arguments
-                                                                .element.USD *
-                                                            arguments
-                                                                .store
-                                                                .state
-                                                                .exchangeRatio) *
-                                                    (double.parse(arguments
-                                                            .element.balance) /
-                                                        pow(10, 18)))
-                                                .toStringAsFixed(2),
-                                        style: TextStyle(
-                                            color: HermezColors.blackTwo,
-                                            fontFamily: 'ModernEra',
-                                            fontWeight: FontWeight.w800,
-                                            fontSize: 32)),
-                                  ])),
-                          SizedBox(height: 10),
-                          Text(
-                              (double.parse(arguments.element.balance) /
-                                          pow(10, 18))
-                                      .toString() +
-                                  " " +
-                                  arguments.element.tokenSymbol,
-                              style: TextStyle(
-                                  fontFamily: 'ModernEra',
-                                  fontWeight: FontWeight.w500,
-                                  color: HermezColors.steel,
-                                  fontSize: 18)),
-                          SizedBox(height: 20),
-                          buildButtonsRow(context),
-                          SizedBox(height: 20),
-                          /*Container(
-                            color: Colors.white,
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.only(left: 30.0, top: 20.0),
-                              child: SizedBox(
-                                width: double.infinity,
-                                child: Text(
-                                  "Activity",
-                                  style: TextStyle(
-                                      fontFamily: 'ModernEra',
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 18),
-                                  textAlign: TextAlign.left,
-                                ),
-                              ),
+                    borderRadius: BorderRadius.all(Radius.circular(5))),
+                padding:
+                    EdgeInsets.only(left: 10, right: 10, top: 7, bottom: 5),
+                child: Text(
+                    arguments.store.state.txLevel == TransactionLevel.LEVEL1
+                        ? "L1"
+                        : "L2",
+                    style: TextStyle(
+                        fontFamily: 'ModernEra',
+                        color: HermezColors.lightOrange,
+                        backgroundColor: Color.fromRGBO(51, 51, 51, 1.0),
+                        fontWeight: FontWeight.w800,
+                        fontSize: 18)),
+              )
+            ],
+          ),
+          backgroundColor: HermezColors.lightOrange,
+          elevation: 0),
+      body: Container(
+        color: HermezColors.lightOrange,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 40),
+            SizedBox(
+                width: double.infinity,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                          (currency == "USD" ? "\$" : "€") +
+                              ((currency == "USD"
+                                          ? arguments.element.USD
+                                          : arguments.element.USD *
+                                              arguments
+                                                  .store.state.exchangeRatio) *
+                                      (double.parse(arguments.element.balance) /
+                                          pow(10, 18)))
+                                  .toStringAsFixed(2),
+                          style: TextStyle(
+                              color: HermezColors.blackTwo,
+                              fontFamily: 'ModernEra',
+                              fontWeight: FontWeight.w800,
+                              fontSize: 32)),
+                    ])),
+            SizedBox(height: 10),
+            Text(
+                (double.parse(arguments.element.balance) / pow(10, 18))
+                        .toString() +
+                    " " +
+                    arguments.element.tokenSymbol,
+                style: TextStyle(
+                    fontFamily: 'ModernEra',
+                    fontWeight: FontWeight.w500,
+                    color: HermezColors.steel,
+                    fontSize: 18)),
+            SizedBox(height: 20),
+            buildButtonsRow(context),
+            SizedBox(height: 20),
+            Expanded(
+                child: FutureBuilder<List<dynamic>>(
+                    future: getTransactions(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<dynamic>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Container(
+                          color: Colors.white,
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              backgroundColor: HermezColors.orange,
                             ),
-                          ),*/
-                          Expanded(
-                            child: Activity(
+                          ),
+                        );
+                      } else {
+                        if (snapshot.hasError) {
+                          // while data is loading:
+                          return Container(
+                            color: Colors.white,
+                            child: Center(
+                              child: Text('There was an error:' +
+                                  snapshot.error.toString()),
+                            ),
+                          );
+                        } else {
+                          if (snapshot.hasData) {
+                            // data loaded:
+                            //supportedTokens = snapshot.data;
+
+                            return Activity(
                               address: arguments.store.state.address,
                               symbol: arguments.element.tokenSymbol,
                               exchangeRate: currency == "USD"
@@ -177,22 +154,22 @@ class WalletAccountDetailsPage extends HookWidget {
                               defaultCurrency:
                                   arguments.store.state.defaultCurrency,
                               cryptoList: snapshot.data,
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  } else {
-                    return Container(
-                      color: Colors.white,
-                      child: Center(
-                        child: Text('There is no data'),
-                      ),
-                    );
-                  }
-                }
-              }
-            }));
+                            );
+                          } else {
+                            return Container(
+                              color: Colors.white,
+                              child: Center(
+                                child: Text('There is no data'),
+                              ),
+                            );
+                          }
+                        }
+                      }
+                    })),
+          ],
+        ),
+      ),
+    );
   }
 
   buildButtonsRow(BuildContext context) {
