bf941031801f5136c8b9150f114a83978cdc2412
Raul Jareño <raul.jareno.diaz@gmail.com>
2021-08-30 14:53:51 +0200

    added mqtt client

---
+++ lib/service/network/mqtt_client.dart
@@ -0,0 +1,69 @@
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+Future<MqttServerClient> connect() async {
+  MqttServerClient client =
+      MqttServerClient.withPort('broker.emqx.io', 'hermez_flutter_app', 1883);
+  client.logging(on: true);
+  client.onConnected = onConnected;
+  client.onDisconnected = onDisconnected;
+  client.onUnsubscribed = onUnsubscribed;
+  client.onSubscribed = onSubscribed;
+  client.onSubscribeFail = onSubscribeFail;
+  client.pongCallback = pong;
+
+  final connMessage = MqttConnectMessage()
+      .authenticateAs('username', 'password')
+      .withWillTopic('willtopic')
+      .withWillMessage('Will message')
+      .startClean()
+      .withWillQos(MqttQos.atLeastOnce);
+  client.connectionMessage = connMessage;
+  client.keepAlivePeriod = 60;
+  try {
+    await client.connect();
+  } catch (e) {
+    print('Exception: $e');
+    client.disconnect();
+  }
+
+  client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+    final MqttPublishMessage message = c[0].payload;
+    final payload =
+        MqttPublishPayload.bytesToStringAsString(message.payload.message);
+
+    print('Received message:$payload from topic: ${c[0].topic}>');
+  });
+
+  return client;
+}
+
+// connection succeeded
+void onConnected() {
+  print('Connected');
+}
+
+// unconnected
+void onDisconnected() {
+  print('Disconnected');
+}
+
+// subscribe to topic succeeded
+void onSubscribed(String topic) {
+  print('Subscribed topic: $topic');
+}
+
+// subscribe to topic failed
+void onSubscribeFail(String topic) {
+  print('Failed to subscribe $topic');
+}
+
+// unsubscribe succeeded
+void onUnsubscribed(String topic) {
+  print('Unsubscribed topic: $topic');
+}
+
+// PING response received
+void pong() {
+  print('Ping response client callback invoked');
+}
